7be342e6661caca49e2b982719520f0de05a1e00
Reza Naser <rezanm2@gmail.com>
2017-12-01 09:17:58 +0100

    Javadoc toegevoegd voor Application klasse

--- src/main/java/nl/webedu/WebEduApiApplication.java
+++ src/main/java/nl/webedu/WebEduApiApplication.java
@@ -5,14 +5,21 @@ import io.dropwizard.ConfiguredBundle;
 import io.dropwizard.bundles.assets.ConfiguredAssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-
+/**
+ * Deze klasse is de startpunt van de api
+ * @author rezanaser
+ */
 public class WebEduApiApplication extends Application<WebEduApiConfiguration> {
     private ConfiguredBundle assetsBundle;
     private String name;
     public static void main(final String[] args) throws Exception {
         new WebEduApiApplication().run(args);
     }
-
+    /**
+     * Deze methode returnt de apiName
+     * De apiName krijgt hij van e WebEduApiConfiguration Klasse
+     * @return -> apiName
+     */
     @Override
     public String getName() {
         return name;
@@ -20,6 +27,11 @@ public class WebEduApiApplication extends Application<WebEduApiConfiguration> {
 
     @Override
     public void initialize(final Bootstrap<WebEduApiConfiguration> bootstrap) {
+        /**
+         * Hier wordt er een assetsBundle aangemaakt en wordt de er gezegd index.html moet zoeken in de angular directory
+         * De path van de angular directory wordt meegeven in het config.yml bestand in de home directory.
+         * @author rezanaser
+         */
         assetsBundle = (ConfiguredBundle) new ConfiguredAssetsBundle("/assets/", "/client", "index.html");
         
         bootstrap.addBundle(assetsBundle);
@@ -28,6 +40,10 @@ public class WebEduApiApplication extends Application<WebEduApiConfiguration> {
     @Override
     public void run(final WebEduApiConfiguration configuration,
                     final Environment environment) {
+        /**
+         * Hier krijgt de variabele 'naam' de apiName van de WebEduApiConfiguration
+         * @author rezanaser
+         */
         name = configuration.getApiName();
     }
 
