653d0d098827726021c6d1015948496f531509a2
RdenBlaauwen <rdenblaauwen@gmail.com>
2017-12-08 11:16:13 +0100

    Auto stash before merge of "EntryResourcemaken" and "origin/master"

--- src/main/java/nl/webedu/resources/EntryResource.java
+++ src/main/java/nl/webedu/resources/EntryResource.java
@@ -1,4 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package nl.webedu.resources;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.ArrayList;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.FormParam;
+import com.google.common.base.Optional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import nl.webedu.dao.EntryDAO;
+import nl.webedu.models.EntryModel;
+
+/**
+ *
+ * @author rezanaser
+ */
+@Path("/entries")
 public class EntryResource {
+    private EntryDAO entryDao;
+
+    public EntryResource(){
+        entryDao = new EntryDAO();
+    }
+
+    
 }
